7ffc9d6a10b4fb37a79102935d3c2cf01216deb4
Ricardo Dani <ricardodani@gmail.com>
2018-02-20 16:37:30 -0300

    Fixed

--- install.sh
+++ install.sh
@@ -1,2 +1,14 @@
-ln -s vimrc ~/.vimrc
-ln -s vimrc.bundles ~/.vimrc.bundles
+mkdir -p ~/.vimrc.backup
+
+if [ -f ~/.vimrc ]; then
+	mv ~/.vimrc ~/.vimrc.backup/
+fi
+
+if [ -f ~/.vimrc.bundles ]; then
+	mv ~/.vimrc.bundles ~/.vimrc.backup/
+fi
+
+ln -Trsf vimrc ~/.vimrc
+ln -Trsf vimrc.bundles ~/.vimrc.bundles
+
+echo "Versioned .vimrc linked successfully. Backup was made on ~/.vimrc.backup"
