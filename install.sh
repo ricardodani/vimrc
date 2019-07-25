2de5796f48c664d8821445fd3e89c1fe08f2c3ff
Ricardo Dani <ricardodani@gmail.com>
2019-07-25 15:42:51 -0300

    Fix install script, adding more verbose information.

--- install.sh
+++ install.sh
@@ -1,26 +1,37 @@
 #!/bin/sh
 
 if [ -f ~/.vim/bundle ]; then
+    echo "Creating plugin directory..."
     mkdir -p ~/.vim/bundle
 fi
-echo "Installing vundle"
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-mkdir -p ~/.vimrc.backup
+if [ ! -d ~/.vim/bundle/Vundle.vim  ]; then
+    echo "Installing vundle..."
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+
+if [ -d ~/.vimrc.backup ]; then
+    echo "Creating backup folder..."
+    mkdir -p ~/.vimrc.backup
+fi
 
 if [ -f ~/.vimrc ]; then
-	mv ~/.vimrc ~/.vimrc.backup/
+    echo "Backing up old vim configuration..."
+    mv ~/.vimrc ~/.vimrc.backup/
 fi
 
 if [ -f ~/.vimrc.bundles ]; then
-	mv ~/.vimrc.bundles ~/.vimrc.backup/
+    echo "Backing up old plugins configuration ..."
+    mv ~/.vimrc.bundles ~/.vimrc.backup/
 fi
 
+echo "Installing .vimrc and .vimrc.bundles..."
 ln -Trsf vimrc ~/.vimrc
 ln -Trsf vimrc.bundles ~/.vimrc.bundles
 
-echo "Installing vundle plugins"
-
+echo "Installing vundle plugins..."
 vim +PluginInstall +qall
 
-echo "Versioned .vimrc linked successfully. Backup was made on ~/.vimrc.backup."
+echo "*** Installation succeded! =) ***"
+echo "Vim configuration located at ~/.vimrc, plugin configuration located at ~/.vimrc.bundles."
+echo "Last backup located at ~/.vimrc.backup"
